1640dc5f73e1f6e6aaa9eff9e4df184e71208d54
BoruiGu <alax919@gmail.com>
2015-03-08 23:19:04 -0400

    Exp week 7

---
+++ essential/experiments/angular/js/10.js
@@ -0,0 +1,18 @@
+﻿var app = angular.module('hoverApp', []);
+
+app.controller('hoverController', function ($scope) {
+    var tableContent = [
+        { a: 1, b: 2, c: 3 },
+        { a: 4, b: 5, c: 6 },
+        { a: 7, b: 8, c: 9 }
+    ];
+
+    $scope.tableContent = tableContent;
+
+    $scope.hover = function (content, event) {
+        console.log(event);
+        if (typeof content.showBtn == 'undefined')
+            content.showBtn = false;
+        content.showBtn = !content.showBtn;
+    };
+});
