2a283805d599a32fa2e7d2a2686d4df3a69dabcf
BoruiGu <alax919@gmail.com>
2015-03-22 15:47:44 -0400

    Exp week 8

---
+++ essential/experiments/Project-Experiments/01.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module("SearchApp", []);
+
+app.controller("SearchCtrl", function ($scope, $http) {
+    $scope.search = function (query) {
+        var queryUrl = "https://api.spotify.com/v1/search?q="+query+"&type=album,artist,track&limit=5";
+        $http.get(queryUrl).success(function (res) {
+            console.log(res);
+            $scope.res = res;
+        });
+    };
+});
---
+++ essential/experiments/Project-Experiments/05.js
@@ -0,0 +1,21 @@
+﻿var app = angular.module("CommuApp", []);
+
+app.controller("CommuCtrl", function ($scope) {
+    var player = new Audio();
+    player.loop = true;
+    player.src = "https://p.scdn.co/mp3-preview/885021ec9b6ac110238a9463bdcc0dc6d4778cec";
+
+    $scope.play = function(){
+        player.play();
+        localStorage.setItem("musicPlaying", Date.now());
+    }
+
+    $scope.pause = function () {
+        player.pause();
+    }
+
+    window.addEventListener('storage', function () {
+        console.log("in event listener");
+        player.pause();
+    });
+});
