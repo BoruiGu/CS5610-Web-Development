53d122e9abf1bd1f88447446a8904a55c08054e7
BoruiGu <alax919@gmail.com>
2015-03-29 14:08:40 -0400

    exp/week9

---
+++ essential/experiments/Project-Experiments/06.js
@@ -0,0 +1,37 @@
+﻿var app = angular.module("SearchApp", []);
+
+app.controller("SearchCtrl", function ($scope, $http) {
+    $scope.search = function (query) {
+        $scope.ACRes = null;
+        search(query, function (res) {
+            $scope.res = res;
+        });
+    };
+
+    $scope.autoComplete = function (query) {
+        $scope.ACRes = null;
+        search(query, function (res) {
+            $scope.ACRes = res;
+        });
+    };
+
+    function search(query, callback) {
+        if (typeof query == 'undefined') {
+            // query is undefined, don't send a search query
+            console.log("invalid query");
+            return;
+        }
+        if (!query) {
+            // query might be: null, undefined, NaN, "", 0, false
+            // don't send a search query
+            console.log("invalid query");
+            return;
+        }
+
+        var queryUrl = "https://api.spotify.com/v1/search?q=" + query + "&type=album,artist,track&limit=5";
+        $http.get(queryUrl).success(function (res) {
+            console.log(res);
+            callback(res);
+        });
+    }
+});
---
+++ essential/experiments/Project-Experiments/07.js
@@ -0,0 +1,28 @@
+﻿var app = angular.module("PlayerApp", []);
+app.controller("PlayerCtrl", function ($scope, $interval) {
+    var player = new Audio();
+    $scope.progress = 0;
+
+    $scope.play = function () {
+        player.loop = true;
+        player.src = "https://p.scdn.co/mp3-preview/885021ec9b6ac110238a9463bdcc0dc6d4778cec";
+        player.addEventListener('loadedmetadata', function () {
+            console.log('player loadedmetadata');
+            $scope.duration = player.duration * 1000;
+            player.play();
+            setTimer();
+        }, false);
+    }
+
+    var timer = 0;
+    function tick() {
+        $scope.progress = Math.floor(player.currentTime * 1000);
+    }
+    function setTimer() {
+        stopTimer();
+        timer = $interval(tick, 100);
+    }
+    function stopTimer() {
+        $interval.cancel(timer);
+    }
+});
