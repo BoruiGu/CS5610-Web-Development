7e58b87847a6707d8f53aee2b21f83eb0b62578c
BoruiGu <alax919@gmail.com>
2015-04-04 20:48:20 -0400

    week10/exp

---
+++ essential/experiments/Project-Experiments/13.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module("InsertApp", []);
+app.controller("InsertCtrl", function ($scope, $http) {
+    $scope.student = {
+        name: "Aoko",
+        major: "MH"
+    };
+    $scope.addStudent = function () {
+        var student = $scope.student;
+        $http.get('http://experiments-borui.rhcloud.com/api/addstudent?name='+student.name+'&major='+student.major);
+    }
+});
